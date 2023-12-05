8b58289e0a0ee86a39250df8311a1259534376eb
jjub0217 <noonssub1004@gmail.com>
2023-12-05 11:59:47 +0900

    배포 준비중

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 $(function () {
 
-
+  /**
+   * @스크롤조금내리면bestmenu가나타나게하는기능
+   * 
+   */
   $(window).scroll(function(){
     curr = $(this).scrollTop()
     if(curr > 0){
@@ -11,10 +14,18 @@ $(function () {
   })
 
 
-  // 어떠한 이벤트가 발생했을때 새로고침시, 해당 이벤트가 그대로 발생되어있게끔 강제로 해주는 기능이 trigger
+  /**
+   * @스크롤트리거
+   * 어떠한 이벤트가 발생했을때 새로고침시, 해당 이벤트가 그대로 발생되어있게끔 강제로 해주는 기능이 trigger
+   */
+  // 
   $(window).trigger("scroll")
 
 
+  /**
+   * @스크롤조금내리면나타나는bestmenu에서전체menu를보기위한화살표버튼눌렀을때전체메뉴가나타나는기능
+   * 
+   */
   $(".header .bottom .all-btn").click(function(){
     $(this).toggleClass("on")
     $(".header .bottom").toggleClass("show")
@@ -44,6 +55,10 @@ $(function () {
     $('.dimmed').show()
     $("body").addClass("scroll-hide")
   })
+  /**
+   * @gnb메뉴닫기
+   * 
+   */
   $('.menu-close-btn, .dimmed').click(function(){
     $('.gnb').removeClass('isAct')
     $('.dimmed').hide()
@@ -58,23 +73,27 @@ $(function () {
   $(".section-best .tab-area .tab-item").click(function(e){
     e.preventDefault()
     tabId = $(this).data('tab');
-    console.log(tabId);
 
     $(this).addClass('on').siblings().removeClass('on');
     $(tabId).addClass('on').siblings().removeClass('on');
   })
 
 
+
   /**
    * @gnb영역내에서서브리스트보여지는기능
    * 
    */
-  $('.nav-item .title').click(function () {
+  $('.nav-item .title').click(function (e) {
+    e.preventDefault();
     $(this).toggleClass("on").siblings('.sub-list').stop().slideToggle()
   })
 
 
-
+  /**
+   * @swiper기능
+   * 
+   */
   const adSlide = new Swiper('.section-ad .swiper',{
     effect: "fade",
     autoplay:{
@@ -84,6 +103,10 @@ $(function () {
   })
   
 
+  /**
+   * @swiper기능
+   * 
+   */
   const mainSlide = new Swiper('.section-visual .swiper',{
     effect: "fade",
     navigation : {
@@ -99,6 +122,11 @@ $(function () {
     loop: true,
   } )
 
+
+  /**
+   * @swiper기능
+   * 
+   */
   const bannerSlide = new Swiper('.section-banner .swiper',{
     pagination: {
       el: ".pagination",
@@ -110,11 +138,15 @@ $(function () {
     loop: true,
   })
   
+
+  /**
+   * @swiper기능
+   * 
+   */
   const bestCateSlide = new Swiper('.section-bestCate .swiper',{
       slidesPerView: 2.2,
       spaceBetween: 10,
   })
-
 
 })
 
